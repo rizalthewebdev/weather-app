5911e0896206b86a711dcbe35f07982a46029d94
rzlTrimelty <rzl.trimelty@gmail.com>
2022-03-21 17:33:03 +0700

    Setup Project

---
+++ src/components/Loading.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactLoading from 'react-loading'
+
+const Loading = () => {
+  return (
+    <ReactLoading type='bars' color='#333333' />
+  )
+}
+
+export default Loading
