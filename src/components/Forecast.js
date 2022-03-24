a748acc674820747640269069c08335e61699205
rzlTrimelty <rzl.trimelty@gmail.com>
2022-03-24 08:45:30 +0700

    Hmm

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -41,7 +41,7 @@ const Forecast = ({ data }) => {
                   drag="x"
                   dragConstraints={{ right: 0, left: -width }}
                   whileTap={{ cursor: "grabbing" }}
-                  className="flex items-center justify-between gap-x-5 cursor-grab"
+                  className="flex items-center justify-between gap-x-5 cursor-grab py-1 px-1"
                >
                   {forecast?.map(
                      (weather, index) =>
